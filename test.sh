8d606058e31f762c9aac252bdc7a4cb7eb6d467d
karoteeni110 <akb48auy@sohu.com>
2019-03-10 00:05:41 +0200

    test.sh fixed

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cat testwordlist.txt | hfst-lookup FSN.hfst | awk '{print $2}' 1>outcome.txt
+while read line; do echo "$line"; done < testwordlist.txt | hfst-lookup FSN.hfst | awk '{print $2}' 1>outcome.txt
 cat outcome.txt | hfst-lookup finntreebank.hfst 1>test_result.txt
 correct=$(cat test_result.txt | grep -F '<V><act><a><sg>' | wc -l )
 all=$(cat outcome.txt | wc -l )
 printf "\nPrecision:\n"
 echo "scale=2 ; $correct / $all" | bc
+echo "correct / all"
+echo $correct / $all
